c328b3cb3184cb2cc2e84ebbb8cd9cc2144569bd
Peter-Adjei <95479664+Peter-Adjei@users.noreply.github.com>
2023-06-13 17:34:26 -0700

    SecondFC

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+// const App = () => {
+//   return (
+//   <>
+//     <h1> “Codetrain is awesome, react works!!!!” </h1>   
+//   </>
+//   )
+// }
+// export default App;
+
+import React, { Component } from 'react';
+class App extends Component{
+  render(){
+    return (
+      <div className='App'>
+       <h1>"Codetrain is awesome, react works!!!!"</h1>
+      </div>  
+    )
+   }
+}
+export default App;
